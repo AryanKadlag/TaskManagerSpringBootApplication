f04a7758c9f9152892c06288447cf28be882e98e
AryanKadlag <aryankadlag20@gmail.com>
2025-02-09 12:21:04 +0530

    Added createTask in Service,Added Repository and added a Unit test for it

--- src/main/java/com/scl/taskmgrspring/tasks/TasksRepository.java
+++ src/main/java/com/scl/taskmgrspring/tasks/TasksRepository.java
@@ -1,4 +1,12 @@
 package com.scl.taskmgrspring.tasks;
 
-public class TasksRepository {
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface TasksRepository extends JpaRepository<TasksEntity, Long> {
+
+
 }
--- src/main/java/com/scl/taskmgrspring/tasks/TasksService.java
+++ src/main/java/com/scl/taskmgrspring/tasks/TasksService.java
@@ -1,4 +1,24 @@
 package com.scl.taskmgrspring.tasks;
 
+import org.springframework.stereotype.Service;
+import java.util.Date;
+
+@Service
 public class TasksService {
+
+    private final TasksRepository tasksRepository;
+
+    public TasksService(TasksRepository tasksRepository) {
+        this.tasksRepository = tasksRepository;
+    }
+
+    public TasksEntity createTask(String title, String description, Date dueDate) {
+        TasksEntity task = new TasksEntity();
+        task.setTitle(title);
+        task.setDescription(description);
+        task.setCompleted(false);
+        task.setDueDate(dueDate);
+        return tasksRepository.save(task);
+
+    }
 }
