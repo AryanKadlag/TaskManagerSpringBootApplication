0fb957f41d2108679b8d851a41ab263f5cecab76
AryanKadlag <aryankadlag20@gmail.com>
2025-02-09 11:24:23 +0530

    Basic MVC Based Project

---
+++ src/main/java/com/scl/taskmgrspring/TaskMgrSpringApplication.java
@@ -0,0 +1,13 @@
+package com.scl.taskmgrspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskMgrSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskMgrSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scl/taskmgrspring/TaskMgrSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scl.taskmgrspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskMgrSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
